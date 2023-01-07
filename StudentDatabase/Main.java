0c077154c11bbe27712a9285b722d353362f32c0
AMEER KHAN B <87577281+ameerkhan123ak@users.noreply.github.com>
2023-01-07 15:48:03 +0530

    Update Main.java

--- StudentDatabase/Main.java
+++ StudentDatabase/Main.java
@@ -17,7 +17,8 @@ public class Main
 			System.out.println("2. Display Student Name");
 			System.out.println("3. display entire Students info");
 			System.out.println("4. Delete Student data");
-			System.out.println("5. Exit");
+			System.out.println("5. Update Student Name");
+			System.out.println("6. Exit");
 			System.out.println("-----------------------");
 			System.out.print("Enter the choice: ");
 			choice = sc.nextInt();
@@ -44,8 +45,16 @@ public class Main
 				int r = sc.nextInt();
 				StudentDatabase.DeleteStudentInfo(r);
 				break;
-				
+			
 			case 5:
+				System.out.print("Enter the name: ");
+				String n = sc.next();
+				System.out.print("Enter the rollno: ");
+				int rl = sc.nextInt();
+				StudentDatabase.UpdateStudentName(rl,n);
+				break;
+				
+			case 6:
 				System.exit(0);
 				
 			default:
